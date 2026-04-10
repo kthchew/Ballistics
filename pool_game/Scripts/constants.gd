22409a23d0f006eeed5effa20fbf6c403fb16c66
Kai Blahnik <83978042+blahnikkai@users.noreply.github.com>
2026-04-10 17:50:51 -0400

    Change BALLS_BEFORE_EIGHT constant to 7.

--- pool_game/Scripts/constants.gd
+++ pool_game/Scripts/constants.gd
@@ -2,6 +2,6 @@ class_name Constants
 
 const BALL_RADIUS: float = 2.85
 # real game = 7, cue + eight game = 0
-const BALLS_BEFORE_EIGHT: int = 1
+const BALLS_BEFORE_EIGHT: int = 7
 const SHAPECAST_LAYER: int = 1 << 2
 const AI_DRAW_AIM_GUIDE: bool = true
