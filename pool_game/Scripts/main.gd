4b076b3859df1a511c39893aa8ea673b11d43299
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2026-04-14 03:03:58 -0400

    Fix issue where cue stick may jump back and forth

--- pool_game/Scripts/main.gd
+++ pool_game/Scripts/main.gd
@@ -812,6 +812,7 @@ func ai_play():
 		
 func start_round(scratched_prev: bool = false) -> void:
 	ball_manager.unfreeze_balls()
+	cue_stick.set_force_strength(0.0)
 	if scratched_prev:
 		if ball_manager.check_eight_ball_potted():
 			end_game(player_ind)
